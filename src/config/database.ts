b63c93394f022501c902e5364f89a301b71267f1
kwabenaaseda <mr.mensahgibson@gmail.com>
2026-08-26 16:21:12 +0000

    Testing builds

--- src/config/database.ts
+++ src/config/database.ts
@@ -4,6 +4,22 @@ dotenv.config();
 
 import { DataSource } from "typeorm";
 import { ENV, VALIDATE_ENV } from "../workers/env_validator.ts";
+import { Candidate } from "../entities/Candidates.ts";
+import { Election } from "../entities/Election.ts";
+import { AuditLog } from "../entities/audit_log.ts";
+import { ColdStore } from "../entities/cold_store.ts";
+import { OrganizationAuth } from "../entities/Org_auth.ts";
+import { OrgStructure } from "../entities/Org_structure.ts";
+import { Organization } from "../entities/Organization.ts";
+import { OrgMemberProfiles } from "../entities/OrgMember_profile.ts";
+import { OrgMemberProfileEdits } from "../entities/OrgMember_profileEdit.ts";
+import { OrgMembers } from "../entities/OrgMembers.ts";
+import { RolePermission } from "../entities/RolePermission.ts";
+import { SystemAdmin } from "../entities/SystemAdmin.ts";
+import { TokenBlacklist } from "../entities/token_blacklist.ts";
+import { User } from "../entities/User.ts";
+import { VoteRecord } from "../entities/Vote_record.ts";
+import { VoteTally } from "../entities/Vote_tally.ts";
 VALIDATE_ENV();
 
 export const AppDataSource = new DataSource({
@@ -16,7 +32,7 @@ export const AppDataSource = new DataSource({
     synchronize: false, // Set to false in production, true for development
     migrationsRun:false, // Set to true if you want migrations to run automatically on app start
     logging: false,
-    entities: ['src/entities/*.ts'],
+    entities: [Candidate,Election,AuditLog,ColdStore,OrganizationAuth, OrgStructure,Organization, OrgMemberProfiles,OrgMemberProfileEdits,OrgMembers,RolePermission,SystemAdmin,TokenBlacklist, User, VoteRecord,VoteTally],
     migrations: ['src/migrations/*.ts'],
     subscribers: [],
 });
